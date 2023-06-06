e13dcc53330e467992e95fc4097e30faf24ca21d
Devin Lin <espidev@gmail.com>
2023-06-06 11:12:18 -0400

    Speed up compile time

--- website/next.config.js
+++ website/next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig = {
   swcMinify: true,
+  modularizeImports: {
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+  },
 }
 
 export default async (phase, {defaultConfig}) => {
-  return nextConfig;
+  return withBundleAnalyzer(nextConfig);
 }
