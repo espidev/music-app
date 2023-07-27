8380f674f5587c05f9347aca3adddfc4ba0b699d
Devin Lin <espidev@gmail.com>
2023-07-26 22:13:45 -0400

    Fix track search

--- website/app/api/collection/[accountUuid]/tracks/search/route.ts
+++ website/app/api/collection/[accountUuid]/tracks/search/route.ts
@@ -5,6 +5,7 @@ import { getAPITrack } from "@/util/models/track";
 import { getAPIAlbum } from "@/util/models/album";
 import { getAPIArtist } from "@/util/models/artist";
 import { getAPIGenre } from "@/util/models/genre";
+import { getAPIPlaylist } from "@/util/models/playlist";
 
 // GET /collections/[accountUuid]/search
 // Search for tracks
@@ -37,7 +38,8 @@ export async function GET(request: Request, { params }: { params: { accountUuid:
     SELECT t.*, 
       JSON_AGG(artist.*) as artists, 
       JSON_AGG(album.*) as albums,
-      JSON_AGG(genre.*) as genres
+      JSON_AGG(genre.*) as genres,
+      JSON_AGG(playlist.*) as playlists
     FROM track as t
       LEFT OUTER JOIN track_to_artist ON t.id = track_to_artist.track_id
       LEFT OUTER JOIN artist ON track_to_artist.artist_id = artist.id
@@ -45,6 +47,8 @@ export async function GET(request: Request, { params }: { params: { accountUuid:
       LEFT OUTER JOIN album ON track_to_album.album_id = album.id
       LEFT OUTER JOIN track_to_genre ON t.id = track_to_genre.track_id
       LEFT OUTER JOIN genre ON track_to_genre.genre_id = genre.id
+      FULL OUTER JOIN playlist_tracks ON t.id = playlist_tracks.track_id
+      FULL OUTER JOIN playlist ON playlist_tracks.playlist_id = playlist.id
       WHERE t.account_uuid = $1::text AND (
         t.name ILIKE $2 OR
         t.artist_display_name ILIKE $2 OR
@@ -63,6 +67,7 @@ export async function GET(request: Request, { params }: { params: { accountUuid:
     apiTrack.albums = track.albums.filter((album: any) => album).map((album: any) => getAPIAlbum(album));
     apiTrack.artists = track.artists.filter((artist: any) => artist).map((artist: any) => getAPIArtist(artist));
     apiTrack.genres = track.genres.filter((genre: any) => genre).map((genre: any) => getAPIGenre(genre));
+    apiTrack.playlists = track.playlists.filter((playlist: any) => playlist).map((playlist: any) => getAPIPlaylist(playlist));
     return apiTrack;
   });
 
